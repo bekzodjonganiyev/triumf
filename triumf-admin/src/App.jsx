3789540495f1385f4cd37627e6e273648806682a
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-04-11 06:46:59 +0500

    added color costimization to tailwind.config.js

--- triumf-admin/src/App.jsx
+++ triumf-admin/src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="bg-red-300">Lorem</div>;
+  return <div className="bg-active font-sans p-10 "></div>;
 }
 
 export default App;
--- triumf-admin/tailwind.config.js
+++ triumf-admin/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,7 +7,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors:{
+      'sidebar': '#FAF2EE',
+      'active': 'rgba(255, 147, 47, 0.2)',
+      'primary': '#7745E0',
+      'secondary': '#FF932F',
+      'ligth_text': 'rgba(35, 32, 42, 0.6)',
+      'bold_text': '#23202A',
+      'gradient': 'linear-gradient(189.52deg, #FFB571 -10.41%, #FA7800 106.43%)'
+    }
   },
   plugins: [],
 }
