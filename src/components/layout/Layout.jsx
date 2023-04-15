8a6f266c65f9c5c293a2585b87acf16d285e91b1
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-04-15 19:04:10 +0500

    deleting class from index.html

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,17 +1,20 @@
+import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 import { Sidebar, Header } from "..";
 
 export const Layout = () => {
   return (
-    <>
-      <Sidebar />
-      <section className="flex flex-col flex-grow">
-        <Header />
-        <main className="px-5 flex-grow overflow-y-scroll custom-scrollbar">
-          <Outlet />
-        </main>
-      </section>
-    </>
+    <Suspense fallback={<h1>Loading</h1>}>
+      <div className="flex w-screen h-screen">
+        <Sidebar />
+        <section className="flex flex-col flex-grow">
+          <Header />
+          <main className="px-5 flex-grow overflow-y-scroll custom-scrollbar">
+            <Outlet />
+          </main>
+        </section>
+      </div>
+    </Suspense>
   );
 };
