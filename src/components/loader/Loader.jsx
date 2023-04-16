86ae4bf719d4d32a9ac5b399b2d92b36e649c06e
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-04-16 20:48:23 +0500

    loader changed

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,6 +1,4 @@
 import { LoadingOutlined } from "@ant-design/icons";
-import { Spin } from "antd";
-import { useEffect } from "react";
 
 export const Loader = () => {
   return (
@@ -17,23 +15,16 @@ export const Loader = () => {
 };
 
 export const FetchingLoader = () => {
-  useEffect(() => {
-    const spin = document.querySelector(".ant-spin-dot");
-    const spinItem = document.querySelectorAll(".ant-spin-dot-item");
-
-    spin.setAttribute("style", "width: 50px; height: 50px");
-
-    spinItem.forEach((item) => {
-      item.setAttribute(
-        "style",
-        "background-color: #FF932F; width: 20px; height: 20px"
-      );
-    });
-  }, []);
 
   return (
     <div className="w-full h-full flex items-center justify-center bg-white">
-      <Spin size="large" />
+      <LoadingOutlined
+        style={{
+          fontSize: 90,
+          color: "#FF932F",
+        }}
+        spin
+      />
     </div>
   );
 };
