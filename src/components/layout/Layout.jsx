2d558b9cee136e6e146112248346c5c22a819ae8
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-04-16 15:44:09 +0500

    wip

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,18 +1,20 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
-import { Sidebar, Header } from "..";
+import { Sidebar, Header, Loader } from "..";
 
 export const Layout = () => {
   return (
-    <div className="flex w-screen h-screen">
-      <Sidebar />
-      <section className="flex flex-col flex-grow">
-        <Header />
-        <main className="px-5 flex-grow overflow-y-scroll custom-scrollbar">
-          <Outlet />
-        </main>
-      </section>
-    </div>
+    <Suspense fallback={<Loader />}>
+      <div className="flex w-screen h-screen">
+        <Sidebar />
+        <section className="flex flex-col flex-grow">
+          <Header />
+          <main className="px-5 flex-grow overflow-y-scroll custom-scrollbar">
+            <Outlet />
+          </main>
+        </section>
+      </div>
+    </Suspense>
   );
 };
