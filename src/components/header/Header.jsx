8d0c44c116b6eb986f9ef29312b6d1634d801050
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-04-19 16:25:32 +0500

    making responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,12 @@ import logo from "../../assets/images/triumf-logo.png"
 
 export const Header = () => {
   return (
-    <header className="w-full bg-hero py-2">
+    <header className="w-full bg-hero py-2 transition-all">
         <div className="container px-8 mx-auto flex items-center justify-between">
             <div>
                 <a href="/"><img src={logo} alt="Triumf logo" className="w-16 h-auto"/></a>
             </div>
-            <ul className="flex w-1/3 justify-between">
+            <ul className="flex min-[1280px]:w-1/3 justify-between max-[1280px]:gap-10 max-md:flex-col">
                 <li><a href="#bizhaqimizda" className="">Biz xaqimizda</a></li>
                 <li><a href="#imkoniyatlar" className="">Imkoniyatlar</a></li>
                 <li><a href="#bizbilanaloqa" className="">Biz bilan aloqa</a></li>
