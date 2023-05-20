ccd86175ba8f066d0a215f99a1a3c3a4c5e7d03d
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-05-20 13:08:16 +0500

    wip

--- src/components/404/ErrorPage.jsx
+++ src/components/404/ErrorPage.jsx
@@ -6,7 +6,7 @@ export const ErrorPage = () => {
   return (
     <div class="section">
       <h1 class="error">404</h1>
-      <div class="page">Ooops!!! The page you are looking for is not found</div>
+      <div class="page">Ooops!!! Siz qidirgan sahifa topilmadi</div>
       <a class="back-home" href="/app">
         Back to home
       </a>
