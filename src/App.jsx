56a064d8f2e0b5f284f695623cdc8f906d17eb73
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2023-05-28 21:14:05 +0500

    orgLetters added

--- src/App.jsx
+++ src/App.jsx
@@ -35,52 +35,21 @@ function App() {
           {/* SUPER USER ROUTES */}
           <Route index element={<></>} />
           <Route path="organizations" element={<Organizations />} />
-          <Route
-            path="organizations/list/:id"
-            element={<OrganizationsList />}
-          />
-          <Route
-            path="organizations/profile/:id"
-            element={<OrganizationsProfile />}
-          >
-            <Route
-              path="statistika"
-              element={
-                <h1 className="text-red-400">Statistika malumotlari bor</h1>
-              }
-            />
-            <Route
-              path="xisob"
-              element={
-                <h1 className="text-red-400">Xisob-kitob malumotlari bor</h1>
-              }
-            />
-            <Route
-              path="arxiv"
-              element={<h1 className="text-red-400">Arxiv malumotlari bor</h1>}
-            />
+          <Route path="organizations/list/:id" element={<OrganizationsList />}  /> 
+          <Route  path="organizations/profile/:id" element={<OrganizationsProfile />} >
+            <Route path="statistika"  element={   <h1 className="text-red-400">Statistika malumotlari bor</h1> } />
+            <Route path="xisob" element={  <h1 className="text-red-400">Xisob-kitob malumotlari bor</h1> } />
+            <Route path="arxiv" element={<h1 className="text-red-400">Arxiv malumotlari bor</h1>} />
           </Route>
 
           <Route path="couriers" element={<Couriers />} />
-          <Route path="couriers/profile/:id" element={<CouriersProfile />}>
-            <Route
-              path="statistika"
-              element={
-                <h1 className="text-red-400">Statistika malumotlari bor</h1>
-              }
-            />
-            <Route
-              path="xisob"
-              element={
-                <h1 className="text-red-400">Xisob-kitob malumotlari bor</h1>
-              }
-            />
-            <Route
-              path="arxiv"
-              element={<h1 className="text-red-400">Arxiv malumotlari bor</h1>}
-            />
-          </Route>
           <Route path="couriers/:id/add_letter" element={<SelectedLetterTable />}/>
+          <Route path="couriers/profile/:id" element={<CouriersProfile />}> 
+            <Route path="statistika" element={   <h1 className="text-red-400">Statistika malumotlari bor</h1> }  />
+            <Route  path="xisob" element={<h1 className="text-red-400">Xisob-kitob malumotlari bor</h1> } />
+            <Route path="arxiv" element={<h1 className="text-red-400">Arxiv malumotlari bor</h1>} />
+          </Route>
+          
           <Route path="statistics" element={<Statistics />} />
           <Route path="incomes" element={<Incomes />} />
           <Route path="archive" element={<Archives />} />
--- src/pages/organizations/OragnizationsList.jsx
+++ src/pages/organizations/OragnizationsList.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { Table } from "antd";
+
+import apiClient from "../../helper/apiClient";
 
 export const OragnizationsList = () => {
+  const { id } = useParams();
+  const [dataSource, setDataSource] = useState([]);
+
+  const orgLetters = useQuery({
+    queryKey: ["orgLetters", id],
+    queryFn: () => apiClient.getAll(`letters?organization_id=${id}`),
+    onSuccess: (res) => {
+      const arr = res.data.map((item, key) => ({
+        key: item.id,
+        order: key + 1,
+        address: item.address,
+        name: item.name,
+        sender: item.receiver_name,
+        status: item.is_delivered ? (
+          <p className="text-[#00B800] font-semibold text-md">Topshirilgan</p>
+        ) : (
+          <p className="text-[#FF3131] font-bold text-md">Topshirilmagan</p>
+        ),
+      }));
+      setDataSource(arr);
+    },
+  });
+
+  const columns = [
+    {
+      title: "#",
+      dataIndex: "order",
+    },
+    {
+      title: "Manzil",
+      dataIndex: "address",
+    },
+    {
+      title: "F.I.SH",
+      dataIndex: "name",
+    },
+    {
+      title: "Jo'natuvchi",
+      dataIndex: "sender",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+  ];
   return (
     <div>
-        Tashkilotning barcha jonatgan xatlari vaqt boyicha jadvalda aks etadi
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        loading={
+          orgLetters.isLoading ||
+          orgLetters.isRefetching ||
+          orgLetters.isFetching
+        }
+        pagination={{
+          pageSize: 10,
+          position: ["bottomCenter"],
+        }}
+      />
     </div>
-  )
-}
+  );
+};
